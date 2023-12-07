b60a82be488b150769d3b398397caf60c9440c44
maelacudini <maelacudini@gmail.com>
2023-12-07 14:08:13 +0100

    fixing minor stuff

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Articles from "./components/home/articles/Articles";
 export default function Home() {
   return (
     <main style={{ margin: '140px 0px 40px 0px' }}>
-      <Articles category={'business'} />
       <Articles category={'science'} />
       <Articles category={'technology'} />
+      <Articles category={'business'} />
     </main>
   )
 }
