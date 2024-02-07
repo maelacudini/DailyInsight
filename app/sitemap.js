9aad0fe67977d59c971ee3c8d31de6ebcd236af1
maelacudini <maelacudini@gmail.com>
2024-02-07 14:38:57 +0100

    mod sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,52 +1,25 @@
-export async function getBusinessPosts() {
-    const business = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API}`)
-    if (!business.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return business.json()
-}
-
-export async function getSciencePosts() {
-    const science = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=${process.env.NEWS_API}`)
-    if (!science.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return science.json()
-}
-
-
-export async function getTechnologyPosts() {
-    const technology = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${process.env.NEWS_API}`)
-    if (!technology.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return technology.json()
-}
-
+import { articlesList } from "./_utils/data"
 
 export default async function sitemap() {
-    const business = await getBusinessPosts();
-    const science = await getSciencePosts();
-    const technology = await getTechnologyPosts();
-    const businessUrl = business.articles.map((post, index) => {
+    const scienceUrl = articlesList[0].categoryArticles.map((article, index) => {
         return {
-            url: `https://daily-insight-eight.vercel.app/business/${index}`,
+            url: `https://daily-insight-eight.vercel.app/${article.category}/${index}`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.6,
         }
     })
-    const scienceUrl = science.articles.map((post, index) => {
+    const technologyUrl = articlesList[1].categoryArticles.map((article, index) => {
         return {
-            url: `https://daily-insight-eight.vercel.app/science/${index}`,
+            url: `https://daily-insight-eight.vercel.app/${article.category}/${index}`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.6,
         }
     })
-    const technologyUrl = technology.articles.map((post, index) => {
+    const sportUrl = articlesList[2].categoryArticles.map((article, index) => {
         return {
-            url: `https://daily-insight-eight.vercel.app/technology/${index}`,
+            url: `https://daily-insight-eight.vercel.app/${article.category}/${index}`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.6,
@@ -66,6 +39,6 @@ export default async function sitemap() {
             changeFrequency: 'monthly',
             priority: 0.8,
         },
-        ...businessUrl, ...scienceUrl, ...technologyUrl
+        ...s, ...scienceUrl, ...technologyUrl, ...sportUrl
     ]
 }
