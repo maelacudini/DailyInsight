826eb2b8f4fdb2f2a224d7c964d5535337ad7924
maelacudini <maelacudini@gmail.com>
2024-02-07 15:22:03 +0100

    mod generali

--- app/_components/home/article/Article.jsx
+++ app/_components/home/article/Article.jsx
@@ -6,16 +6,14 @@ export default function Article({ article, index, category }) {
   return (
     <article className={style.article}>
       <Link href={`/${category}/${index}`}>
-        <div className={style.imagecont}>
-          <Image
-            src={article.image}
-            alt="image"
-            height={200}
-            width={200}
-            loading={index === 0 ? "eager" : "lazy"}
-            priority={index === 0 ? true : false}
-          />
-        </div>
+        <Image
+          src={article.image}
+          alt="image"
+          height={200}
+          width={200}
+          loading={index === 0 ? "eager" : "lazy"}
+          priority={index === 0 ? true : false}
+        />
 
         <p>{article.title.slice(0, 60)}...</p>
       </Link>
